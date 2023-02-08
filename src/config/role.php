267ce27f05e77b1145b1c2159d0181f0a61c1419
walkerchiu <chenjen.chiou@gmail.com>
2023-02-08 21:15:56 +0800

    Chore(Vendor): Rename "php-site" to "php-site-mall"

--- src/config/role.php
+++ src/config/role.php
@@ -34,7 +34,7 @@ return [
         'morph-image'    => 0,
         'rule'           => 0,
         'rule-hit'       => 0,
-        'site'           => 0
+        'site-mall'      => 0,
     ],
 
     /*
